b59c273f2fd762c7c64db88210d1937eb972e69f
max-shymanskiy <maxshymanskiy@gmail.com>
2024-10-15 09:46:21 +0300

    first commit

---
+++ LR_06_2_rec.cpp
@@ -0,0 +1,64 @@
+﻿#include <iomanip>
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+
+void Create(int* a, const int size, const int Low, const int High, int i)
+{
+	a[i] = Low + rand() % (High - Low + 1);
+	if (i < size - 1)
+		Create(a, size, Low, High, i + 1);
+}
+
+int avgSum(const int* arr, const int size, int i, int s, int count)
+{
+	if (i == size) {
+		if (count == 0) {
+			cerr << "Error: No even numbers in the array." << endl;
+			return 0;
+		}
+		return s / count;
+	}
+	if (arr[i] % 2 == 0) {
+		s += i;
+		count++;
+	}
+	return avgSum(arr, size, i + 1, s, count);
+}
+
+void Print(const int* arr, const int size, int i)
+{
+	if (i < size) {
+		cout << setw(4) << arr[i];
+		Print(arr, size, i + 1);
+	}
+	else {
+		cout << endl;
+	}
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	int size = 16;
+	int Low = 1;
+	int High = 100;
+	int* arr = new int[size];
+
+	Create(arr, size, Low, High, 0);
+
+	cout << "arr[" << size << "] = ";
+
+	Print(arr, size, 0);
+
+	int s = 0;
+	int count = 0;
+	int sum = avgSum(arr, size, 0, s, count);
+	cout << "avgSum: " << sum << endl;
+
+	delete[] arr;
+
+	return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LR_06_2_rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+        TEST_METHOD(TestingZero)
+        {
+            const int size = 3;
+            int arr[size] = { 1, 3, 5 }; 
+            int result = avgSum(arr, size, 0, 0, 0);
+            Assert::AreEqual(0, result);
+        }
+
+        TEST_METHOD(avgSum_Testing)
+        {
+            const int size = 5;
+            int arr[size] = { 2, 3, 4, 5, 6 }; 
+            // expected avgSum = 6 / 3 = 2
+            int result = avgSum(arr, size, 0, 0, 0);
+            Assert::AreEqual(2, result);
+        }
+	};
+}
